b86da4928d598fe150095808903c80339ade67df
Abderahim <ita27rmp100@gmail.com>
2025-02-27 22:50:07 +0100

    update on PyCSV ... Complete creating the tool with example

--- excelToHtml/app.py
+++ excelToHtml/app.py
@@ -1,6 +1,19 @@
 from PyCSV import main
-import pandas as pd
-
-# import file.excel and convert it to file.csv
-df = main.data("file.csv")
-print(df)
+# WELCOME TO SCRIPT & INPUT FILE NAME
+fileName = input("Enter excel file (without .csv) : ")
+# import file.eDATAcel and convert it to file.csv
+try :
+    data = main.dataArrRows(f"{fileName}.csv")
+    htmlTable = '<table>\n'
+    for i in data :
+        row = '<tr>'
+        for j in i :
+            row += f"<td>{j}</td>\n"
+        row += "</tr>\n"  
+        htmlTable += row
+    htmlTable += "</table>"
+    with open(f"{fileName}_TABLE.html",mode="w") as html :
+        html.write(htmlTable)
+    print("DONE ...")
+except :
+    print("ERROR, FILENAME DOESN'T EXIST OR CANNOT BE TREATED NOW")
