465b39edac520348ffbe8bcd91b21560168e322a
Abderahim <ita27rmp100@gmail.com>
2025-03-06 14:37:42 +0100

    edit the moduls of main.c

--- encoding/main.c
+++ encoding/main.c
@@ -4,43 +4,11 @@
 
 // A function To convert dec to bin (declaration)
 int decTo_bin_or_oct(int dec);
-
+void fileToBin();
 // main program
 int main(){
-    // opening a file
-    FILE *text_file ;
-    FILE *new_file ;
-    // set reading mode 
-        // Asking about the name of file :
-        char file_name[30];
-        printf("Enter the file's name : ") ;scanf("%s",&file_name);
-        char BinFile[45];
-        sprintf(BinFile,"bin_%s",file_name);
-        // start reading and writing
-        text_file = fopen(file_name,"r");
-        if (!(text_file)){printf("FILE DOESN'T EXIST ... END PROCCESSING");}
-        else{
-            // start written
-            new_file = fopen(BinFile,"w");
-            // reading the content of the file
-            char ch;
-            while ((ch = fgetc(text_file)) != EOF) {
-                // EOF = the char isn't an ASCII value
-                int CharBin = decTo_bin_or_oct(ch+0);
-                char BinStr[7];
-                char space = ' ';
-                itoa(CharBin,BinStr,10);
-                if (ch+0 !=27){
-                    strncat(BinStr," ",1);
-                }
-                CharBin==1010 ? fprintf(new_file,"\n") : fprintf(new_file,BinStr);
-            }
-            // closing the file
-            fclose(text_file);
-            fclose(new_file);
-            printf("Conversion Completed ... (Resualt stored in : %s _bin.txt)",BinFile);
-        }
     
+    fileToBin();
     return 0;
 }
 
@@ -56,4 +24,40 @@ int decTo_bin_or_oct(int dec){
         dec = (int)dec/2;
     }
     return b;
+}
+// represent files in binary langauge
+void fileToBin(){
+    // opening a file
+    FILE *text_file ;
+    FILE *new_file ;
+    // set reading mode 
+    // Asking about the name of file :
+    char file_name[30];
+    printf("Enter the file's name : ") ;scanf("%s",&file_name);
+    char BinFile[45];
+    sprintf(BinFile,"bin_%s",file_name);
+    // start reading and writing
+    text_file = fopen(file_name,"r");
+    if (!(text_file)){printf("FILE DOESN'T EXIST ... END PROCCESSING");}
+    else{
+        // start written
+        new_file = fopen(BinFile,"w");
+        // reading the content of the file
+        char ch;
+        while ((ch = fgetc(text_file)) != EOF) {
+            // EOF = the char isn't an ASCII value
+            int CharBin = decTo_bin_or_oct(ch+0);
+            char BinStr[7];
+            char space = ' ';
+            itoa(CharBin,BinStr,10);
+            if (ch+0 !=27){
+                strncat(BinStr," ",1);
+            }
+            CharBin==1010 ? fprintf(new_file,"\n") : fprintf(new_file,BinStr);
+        }
+        // closing the file
+        fclose(text_file);
+        fclose(new_file);
+        printf("Conversion Completed ... (Resualt stored in : %s _bin.txt)",BinFile);
+    }
 }
---
+++ encoding/main.py
@@ -0,0 +1,5 @@
+import ctypes
+
+convertor = ctypes.CDLL('./convert.so')
+
+
