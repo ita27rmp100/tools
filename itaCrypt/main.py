ac1f90ab63c031014717cc5439a14b8cb17daa78
Abderahim <ita27rmp100@gmail.com>
2025-03-07 16:33:15 +0100

    itaEncrypt : start make it with python also

--- itaCrypt/main.py
+++ itaCrypt/main.py
@@ -1,22 +1,14 @@
 # same encrypting system with python
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 def decimal_to_binary(n):
-    return f"{n:07b}"
+    return format(n, "07b")
 with open('text.txt','r') as file :
     content = file.read()
     BinCONTENT = ''
     for i in content :
-        if i in letters :
-            if i in letters[0:25] :
-                BinChar = letters.index(i) + 65
-            elif i in letters[26:51] :
-                BinChar = letters.index(i) + 97
-            else :
-                BinChar = letters.index(i) + 48
-            BinChar = decimal_to_binary(BinChar)
-            print(BinChar)
-            BinCONTENT += str(BinChar) + ' '
-            BinChar = 
+        if ord(i) in list(range(65,90)) + list(range(97,122)) + list(range(48,75)) :
+            BinCONTENT += str(decimal_to_binary(ord(i))) + ' '
+            # BinChar = 
         else :
             BinCONTENT += i
 print(BinCONTENT)
