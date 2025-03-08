f7e0851bda3df20a3ead14933b3c39def10aefd6
Abderahim <ita27rmp100@gmail.com>
2025-03-08 20:10:26 +0100

    edit on the system on encrypting in itaEncrypt

--- itaCrypt/main.py
+++ itaCrypt/main.py
@@ -1,22 +1,16 @@
 # same encrypting system with python
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-def decimal_to_binary(n):
-    return format(n, "07b")
-fileName = input("Enter the file's name :")
+letters = "Aa#Bb!Cc%Dd&Ee~@"
+decimal_to_binary = lambda n: bin(n)[2:]
+fileName = input("Enter the file's name : ")
 with open(fileName,'r') as file :
     try :
         content = file.read()
-        BinCONTENT = ''
         bc = ''
         for i in content :
             if ord(i) in list(range(65,90)) + list(range(97,122)) + list(range(48,75)) :
-                BinCONTENT += str(decimal_to_binary(ord(i))) + ' '
                 BinChar =  decimal_to_binary(ord(i))
-                BinChar = int(BinChar[0:3],2) + int(BinChar[3:],2)
-                bc += letters[BinChar]
-                # BinChar = 
+                bc += letters[int(BinChar[0:3],2)] + letters[int(BinChar[3:],2)] + '.'
             else :
-                BinCONTENT += i
                 bc += i
         newFileName = fileName[:fileName.index('.')] + 'Encrypted' + fileName[fileName.index('.'):]
         open(newFileName,'w').write(bc)
