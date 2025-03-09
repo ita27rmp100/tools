7b3fcd869a7e12c178d732e9dfc2105a5be51e31
Abderahim <ita27rmp100@gmail.com>
2025-03-09 14:02:53 +0100

    itaCrypt ... Almost done with python

--- itaCrypt/reverse.py
+++ itaCrypt/reverse.py
@@ -4,12 +4,14 @@ fileName = input("Enter the encrypted file name : ")
 with open(fileName,'r') as file :
     try :
         content = file.read()
-        bc = ''
+        OriginContent = ''
         i = 1
-        while i != len(content) :
+        while i <= len(content) :
             step = content[i:content.index('.')-1]
-            if (len(step) == 2) and (step[0] and step[0] in letters) :
-                pass
-        print("Conversion Completed ... (Result stored in: ",newFileName+")")
+            if (len(step) == 2) and (step[0] and step[1] in letters) :
+                ascii = (bin(letters.index(step[0]))[2:]).zfill(3) + (bin(letters.index(step[0]))[2:]).zfill(4)
+                OriginContent += ascii
+            i += 1
+        print(OriginContent)
     except :
         print("FILE DOESN'T EXIST ... END PROCESSING\n")
