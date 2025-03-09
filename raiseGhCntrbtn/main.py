81ea4f07bb1e93ea30508644e984abb41a54da1b
Abderahim <ita27rmp100@gmail.com>
2025-03-09 17:25:40 +0100

    start creating new tool

---
+++ raiseGhCntrbtn/main.py
@@ -0,0 +1,9 @@
+# Illegal tool to raise your contributions in GitHub
+import os
+
+repeat = input("How many contributions you want to add to ur account ? ")
+with open('appp.py','w') as file :
+    
+os.system('git branch')
+# p = os.system('git status')
+# print(p)
