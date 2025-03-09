2035685ffe11684f3d3744dddab5236489ebe432
Abderahim <ita27rmp100@gmail.com>
2025-03-09 17:32:09 +0100

    almost done

--- raiseGhCntrbtn/main.py
+++ raiseGhCntrbtn/main.py
@@ -2,8 +2,11 @@
 import os
 
 repeat = input("How many contributions you want to add to ur account ? ")
-with open('appp.py','w') as file :
-    
-os.system('git branch')
-# p = os.system('git status')
-# print(p)
+with open('appp.py','a') as file :
+    while repeat != 0 :
+        file.write(f'print({repeat})')
+        repeat -=1
+        os.system('git add .')
+        os.system(f"git commit -m 'commit : {abs(0-repeat)}'")
+        os.system('git push')
+        print("done : ",repeat)
