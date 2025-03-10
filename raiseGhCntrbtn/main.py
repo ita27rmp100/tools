6048a70da262c17b780209f79a3f990e974bdc9b
Abderahim <ita27rmp100@gmail.com>
2025-03-10 08:21:16 +0100

    commit : 2

--- raiseGhCntrbtn/main.py
+++ raiseGhCntrbtn/main.py
@@ -6,12 +6,12 @@ path = input('Enter the path that you wanna push from it : ')
 os.chdir(path)
 
 while repeat != 0 :
-    with open('app.py','w') as file :
+    with open('app.py','a') as file :
         file.write('print("hello world")\n')
     os.system(f'git commit -a -m "commit : {repeat}"')
     os.system("git push")
     print("done : ",repeat)
     repeat -=1
-    
+
 os.system('git commit -a -m "Ending inflating hhhh"')
 os.system("git push")
