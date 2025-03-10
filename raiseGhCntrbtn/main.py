da0b2145d358ed6fdaa3d8fedb8e03027523e45e
Abderahim <ita27rmp100@gmail.com>
2025-03-10 08:20:02 +0100

    commit : 4

--- raiseGhCntrbtn/main.py
+++ raiseGhCntrbtn/main.py
@@ -4,12 +4,14 @@ import os , time
 repeat = int(input("How many contributions you want to add to ur account ? "))
 path = input('Enter the path that you wanna push from it : ')
 os.chdir(path)
-with open('app.py','w') as file :
-    while repeat != 0 :
+
+while repeat != 0 :
+    with open('app.py','w') as file :
         file.write('print("hello world")\n')
-        os.system(f'git commit -a -m "commit : {repeat}"')
-        os.system("git push")
-        print("done : ",repeat)
-        repeat -=1
+    os.system(f'git commit -a -m "commit : {repeat}"')
+    os.system("git push")
+    print("done : ",repeat)
+    repeat -=1
+    
 os.system('git commit -a -m "Ending inflating hhhh"')
 os.system("git push")
