d03fc08f9f0026976f4dc86267362591c52a0ffc
Abderahim <ita27rmp100@gmail.com>
2025-03-14 18:34:33 +0100

    git_push : add push.py (facilate pushing)

---
+++ git_push/push.py
@@ -0,0 +1,6 @@
+import os
+os.chdir(input('Enter the work directory you wanna push it : '))
+os.system("git add .")
+commit = input("Enter commit message : ")
+os.system(f"git commit -m '{commit}'")
+os.system('git push')
