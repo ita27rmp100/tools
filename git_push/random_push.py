da07084899724cce75854664eb97afc32d5ce293
Abderahim <ita27rmp100@gmail.com>
2025-03-17 14:15:54 +0100

    commit : 21

---
+++ git_push/random_push.py
@@ -0,0 +1,19 @@
+import datetime , os 
+from random import random
+
+path = input('Enter the path that you wanna push from it : ')
+
+repeat = int(random()*datetime.datetime.now().day)*datetime.datetime.now().month
+print(repeat)
+while repeat != 0 :
+    with open('app.py','a') as file :
+        file.write('print("hello world")\n')
+    os.system("git add .")
+    os.system(f'git commit -m "commit : {repeat}"')
+    os.system("git push")
+    print("************* done : ",repeat)
+    repeat -=1
+
+os.remove("app.py")
+os.system('git commit -a -m "Ending inflating :)"')
+os.system("git push")
