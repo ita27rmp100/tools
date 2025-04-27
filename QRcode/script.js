177309de9bca906051704e74e73bccf36b331c3e
Abderahim <ita27rmp100@gmail.com>
2025-04-27 09:35:09 +0100

    some changes on css and js code of QRcode tool

--- QRcode/script.js
+++ QRcode/script.js
@@ -1,12 +1,18 @@
 window.onload = () =>{
     document.getElementById('site').focus()
 }
-function generatoring() {
-    let url = document.getElementById('site').value
-    if (url!='') {
-        document.getElementById('QR').src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`
-        document.getElementById('nav').style.height = '425px'
+async function generatoring() {
+    const url = document.getElementById('site').value;
+    const qr = document.getElementById('QR');
+    const nav = document.getElementById('nav');
+    if (url !== '') {
+      const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(url)}`;
+      qr.src = qrUrl;
+      await new Promise((resolve) => {
+        qr.onload = () => resolve(); // wait until image fully loaded
+      });
+      nav.style.height = '425px';
     } else {
-        alert('write url please')
+      alert('write url please');
     }
 }
